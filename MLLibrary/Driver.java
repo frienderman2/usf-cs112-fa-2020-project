81c92bbd6ae8c9f0a879184a44836ec9ae5381c9
callaghandonnelly <donnellycal@gmail.com>
2020-10-19 23:03:22 -0400

    Commit Message

--- MLLibrary/Driver.java
+++ MLLibrary/Driver.java
@@ -1,6 +1,7 @@
 import java.util.*;
+import javax.swing.*;
 public class Driver {
-    public void main(String[] args){
+    public static void main(String[] args){
 
         ArrayList<DataPoint> trainSet = new ArrayList<DataPoint>();
         trainSet.add(0, new DataPoint(1, 2, "red", "red"));
@@ -9,14 +10,19 @@ public class Driver {
         trainSet.add(3, new DataPoint(4, 5, "blue", "blue"));
 
         ArrayList<DataPoint> testSet = new ArrayList<DataPoint>();
-        trainSet.add(0, new DataPoint(2.5, 3.5, "", ""));
+        testSet.add(0, new DataPoint(2.5, 3.5, "", ""));
         //trainSet.add(1, new DataPoint(1.1, 2, "", ""));
 
         DummyModel modelD = new DummyModel();
         modelD.train(trainSet);
 
         String result = modelD.test(testSet);
-        System.out.println(result);
+        JFrame pane = new JFrame("Headline");
+        pane.setSize(100, 100);
+        JLabel lblresult = new JLabel(result);
+        pane.add(lblresult);
+        pane.setVisible(true);
+        pane.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
     }
 }
