85c78c07404b5e76b8db2c5adc1882b42f2bb173
callaghandonnelly <donnellycal@gmail.com>
2020-11-12 03:38:35 -0500

    Proj Pt2

--- MLLibrary/Driver.java
+++ MLLibrary/Driver.java
@@ -1,28 +1,99 @@
-import java.util.*;
 import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.io.*;
+import java.util.List;
+
 public class Driver {
+    // Helper function to split the line by commas and
+    // return the values as a List of String
+    private List<String> getRecordFromLine(String line) {
+        List<String> values = new ArrayList<String>();
+        try (Scanner rowScanner = new Scanner(line)) {
+            rowScanner.useDelimiter(",");
+            while (rowScanner.hasNext()) {
+                values.add(rowScanner.next());
+            }
+        }
+        return values;
+    }
+
+    public void SetUp(ArrayList<DataPoint> testList, ArrayList<DataPoint> trainList, ArrayList<DataPoint> bigList) {
+        try {
+            Scanner read = new Scanner(new File("titanic.csv"));
+            int j = 0;
+            while (read.hasNextLine()) {
+                List<String> records = getRecordFromLine(read.nextLine());
+                if (j > 0){
+                    // Select the columns from the records and create a DataPoint object
+                    String survival = records.get(1);
+                    // System.out.println(records.get(5));
+                    double fare;
+                    try {
+                        fare = Double.parseDouble(records.get(6));
+                    } catch (IndexOutOfBoundsException e){
+                        fare = 0;
+                    }
+                    double age;
+                    try {
+                        age = Double.parseDouble(records.get(5));
+                    }catch (NumberFormatException e){
+                        age = 0;
+                    }
+
+                    DataPoint example = new DataPoint(age, fare, survival, "");
+
+                    Random rand = new Random();
+                    double randNum = rand.nextDouble();
+                    // 90% of the data is reserved for training
+                    if (randNum < 0.9) {
+                        // Set the type of DataPoint as “train” and put into its Collection
+                        example.setType("train");
+                        trainList.add(example);
+                    } else {
+                        // Set the type of DataPoint as “test” and put into its Collection
+                        example.setType("test");
+                        testList.add(example);
+                    }
+                    // Add new data point to the overarching collection in case I need it later
+                    bigList.add(example);
+                }
+                j++;
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void dataSplit() {
+
+    }
+
     public static void main(String[] args){
-
         ArrayList<DataPoint> trainSet = new ArrayList<DataPoint>();
-        trainSet.add(0, new DataPoint(1, 2, "red", "red"));
-        trainSet.add(1, new DataPoint(2, 3, "blue", "blue"));
-        trainSet.add(2, new DataPoint(3, 4, "red", "red"));
-        trainSet.add(3, new DataPoint(4, 5, "blue", "blue"));
-
         ArrayList<DataPoint> testSet = new ArrayList<DataPoint>();
-        testSet.add(0, new DataPoint(2.5, 3.5, "", ""));
-        //trainSet.add(1, new DataPoint(1.1, 2, "", ""));
+        ArrayList<DataPoint> wholeSet = new ArrayList<DataPoint>();
 
-        DummyModel modelD = new DummyModel();
-        modelD.train(trainSet);
+        Driver thing = new Driver();
+        thing.SetUp(testSet, trainSet, wholeSet);
 
-        String result = modelD.test(testSet);
-        JFrame pane = new JFrame("Headline");
-        pane.setSize(100, 100);
-        JLabel lblresult = new JLabel(result);
-        pane.add(lblresult);
-        pane.setVisible(true);
-        pane.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        KNNModel modelK = new KNNModel(3);
+        modelK.train(wholeSet);
+
+        Double acc = modelK.getAccuracy(wholeSet) * 100;
+        Double pre = modelK.getAccuracy(wholeSet) * 100;
+
+        JFrame outline = new JFrame();
+        Container stuffArea = outline.getContentPane();
+        stuffArea.setLayout(new GridLayout(2,1));
+        outline.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        stuffArea.add(new JLabel(String.valueOf(acc)));
+        stuffArea.add(new JLabel(String.valueOf(pre)));
+        outline.pack();
+        outline.setVisible(true);
+
 
     }
 }
